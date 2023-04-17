b7a8d5e15e5e174fb308fa3659e152caf263af02
2bc4 <119853089+2bc4@users.noreply.github.com>
2023-04-17 17:25:11 +0000

    chore: bump user agent

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,7 @@ mod playlist;
 use iothread::IOThread;
 use playlist::MediaPlaylist;
 
-const USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36";
+const USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36";
 
 #[derive(Parser)]
 #[command(version, next_line_help = true)]
