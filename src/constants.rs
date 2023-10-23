2148b93ec76cf16d240b39f62c9f79447d737651
2bc4 <119853089+2bc4@users.noreply.github.com>
2023-10-23 07:53:20 +0000

    Actually git add the file

---
+++ src/constants.rs
@@ -0,0 +1,8 @@
+pub const USER_AGENT: &str =
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0";
+
+pub const TWITCH_GQL_ENDPOINT: &str = "https://gql.twitch.tv/gql";
+pub const TWITCH_OAUTH_ENDPOINT: &str = "https://id.twitch.tv/oauth2/validate";
+pub const TWITCH_HLS_BASE: &str = "https://usher.ttvnw.net/api/channel/hls/";
+
+pub const DEFAULT_CLIENT_ID: &str = "kimne78kx3ncx6brgo4mv6wki5h1ko";
