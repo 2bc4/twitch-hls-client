e51ff65145490353f40caa1c9855ba90299b08a4
2bc4 <119853089+2bc4@users.noreply.github.com>
2023-10-25 14:46:35 +0000

    Release 0.5.4

--- src/http.rs
+++ src/http.rs
@@ -231,7 +231,7 @@ impl Transport {
     #[cfg(feature = "rustls-webpki")]
     fn root_certs() -> rustls::RootCertStore {
         let mut roots = rustls::RootCertStore::empty();
-        roots.add_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(|ta| {
+        roots.add_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.iter().map(|ta| {
             rustls::OwnedTrustAnchor::from_subject_spki_name_constraints(
                 ta.subject,
                 ta.spki,
