0675357bb0b1f4ab0abcbb4f41446c292336216a
2bc4 <affett0@proton.me>
2024-10-09 16:27:25 +0000

    cache: add and verify magic value in cache files

--- src/hls/cache.rs
+++ src/hls/cache.rs
@@ -1,44 +1,56 @@
-use std::{fs, time::Duration};
+use std::{
+    fs::{self, File, ReadDir},
+    io::{Read, Write},
+    path::{Path, PathBuf},
+    time::Duration,
+};
 
+use anyhow::{bail, Result};
 use log::{debug, error};
 
 use crate::http::{Agent, Connection, Url};
 
 pub struct Cache {
-    path: String,
+    path: PathBuf,
 }
 
 impl Cache {
-    pub fn new(dir: &Option<String>, channel: &str, quality: &Option<String>) -> Option<Self> {
-        if let Some(dir) = dir {
-            if let Some(quality) = quality {
-                match fs::metadata(dir) {
-                    Ok(metadata) if metadata.is_dir() && !metadata.permissions().readonly() => {
-                        Self::remove_stale(dir);
+    const MAGIC: &str = concat!(env!("CARGO_PKG_NAME"), "\n");
 
-                        return Some(Self {
-                            path: format!("{dir}/{channel}-{quality}"),
-                        });
-                    }
-                    Err(e) => error!("Failed to open playlist cache directory: {e}"),
-                    _ => error!("Playlist cache path is not writable or is not a directory"),
+    pub fn new(dir: &Option<String>, channel: &str, quality: &Option<String>) -> Option<Self> {
+        let (dir, quality) = dir.as_ref().zip(quality.as_ref())?;
+
+        match Self::read_dir(dir) {
+            Ok(iter) => {
+                for entry in iter {
+                    let Ok(entry) = entry else {
+                        continue;
+                    };
+
+                    Self::remove_if_stale(&entry.path());
                 }
+            }
+            Err(e) => {
+                error!("Failed to read playlist cache directory: {e}");
+                return None;
             }
         }
 
-        None
+        Some(Self {
+            path: format!("{dir}/{channel}-{quality}").into(),
+        })
     }
 
     pub fn get(&self, agent: &Agent) -> Option<Connection> {
-        debug!("Reading playlist cache: {}", self.path);
+        debug!("Trying playlist cache: {}", self.path.display());
 
-        let url = fs::read_to_string(&self.path).ok()?.trim_end().into();
+        let mut file = Self::check_magic(&self.path)?;
+        let mut string = String::new();
+        file.read_to_string(&mut string).ok()?;
+
+        let url = string.into();
         let Some(request) = agent.exists(&url) else {
-            debug!("Removing playlist cache: {}", self.path);
-            if let Err(e) = fs::remove_file(&self.path) {
-                error!("Failed to remove playlist cache: {e}");
-            }
-
+            Self::remove_cache(&self.path);
             return None;
         };
 
@@ -46,39 +58,53 @@ impl Cache {
     }
 
     pub fn create(&self, url: &Url) {
-        debug!("Creating playlist cache: {}", self.path);
-        if let Err(e) = fs::write(&self.path, url.as_str()) {
+        debug!("Creating playlist cache: {}", self.path.display());
+
+        let file = File::create_new(&self.path);
+        if let Err(e) = file.and_then(|mut f| write!(f, "{}{url}", Self::MAGIC)) {
             error!("Failed to create playlist cache: {e}");
         }
     }
 
-    fn remove_stale(dir: &str) {
-        let iter = match fs::read_dir(dir) {
-            Ok(iter) => iter,
-            Err(e) => {
-                error!("Failed to read playlist cache directory: {e}");
-                return;
-            }
-        };
-
-        for entry in iter {
-            let Ok(entry) = entry else {
-                continue;
-            };
-
-            if let Some(duration) = fs::metadata(entry.path())
-                .ok()
-                .and_then(|m| m.modified().ok())
-                .and_then(|t| t.elapsed().ok())
-            {
-                //After 48 hours a playlist cannot be valid
-                if duration >= Duration::from_secs(48 * 60 * 60) {
-                    debug!("Removing stale playlist cache: {}", entry.path().display());
-                    if let Err(e) = fs::remove_file(entry.path()) {
-                        error!("Failed to remove stale playlist cache: {e}");
-                    }
-                }
-            }
+    fn read_dir(dir: &str) -> Result<ReadDir> {
+        let metadata = fs::metadata(dir)?;
+        if !metadata.is_dir() || metadata.permissions().readonly() {
+            bail!("Playlist cache path isn't a directory or is read only");
         }
+
+        Ok(fs::read_dir(dir)?)
+    }
+
+    fn check_magic(path: &Path) -> Option<File> {
+        let mut file = File::open(path).ok()?;
+        let mut buf = [0u8; Self::MAGIC.len()];
+
+        file.read_exact(&mut buf).ok()?;
+        if buf != Self::MAGIC.as_bytes() {
+            return None;
+        }
+
+        Some(file)
+    }
+
+    fn remove_cache(path: &Path) {
+        debug!("Removing playlist cache: {}", path.display());
+        if let Err(e) = fs::remove_file(path) {
+            error!("Failed to remove playlist cache: {e}");
+        }
+    }
+
+    fn remove_if_stale(path: &Path) -> Option<()> {
+        const FOURTY_EIGHT_HOURS: Duration = Duration::from_secs(48 * 60 * 60);
+
+        Self::check_magic(path)?;
+
+        let metadata = fs::metadata(path).ok()?;
+        let modified = metadata.modified().ok().and_then(|t| t.elapsed().ok())?;
+        if metadata.is_file() && modified >= FOURTY_EIGHT_HOURS {
+            Self::remove_cache(path);
+        }
+
+        Some(())
     }
 }
