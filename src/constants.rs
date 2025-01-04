c4068b05249225e4ae7e3f4cf583fbcd40c4c7de
2bc4 <affett0@proton.me>
2025-01-04 20:57:10 +0000

    constants: bump and use CARGO_PKG_NAME for default config path

--- src/constants.rs
+++ src/constants.rs
@@ -1,11 +1,11 @@
 pub const USER_AGENT: &str =
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0";
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0";
 
-pub const PLAYER_VERSION: &str = "1.33.0-rc.5";
+pub const PLAYER_VERSION: &str = "1.35.0-rc.2";
 
 pub const TWITCH_GQL_ENDPOINT: &str = "https://gql.twitch.tv/gql";
 pub const TWITCH_OAUTH_ENDPOINT: &str = "https://id.twitch.tv/oauth2/validate";
 pub const TWITCH_HLS_BASE: &str = "https://usher.ttvnw.net/api/channel/hls/";
 
 pub const DEFAULT_CLIENT_ID: &str = "kimne78kx3ncx6brgo4mv6wki5h1ko";
-pub const DEFAULT_CONFIG_PATH: &str = "twitch-hls-client/config";
+pub const DEFAULT_CONFIG_PATH: &str = concat!(env!("CARGO_PKG_NAME"), "/config");
