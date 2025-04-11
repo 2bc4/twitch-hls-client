28832ea577210e4d91be94dd8bd758d4addd1ad8
2bc4 <affett0@proton.me>
2025-04-11 16:47:54 +0000

    constants: bump

--- src/constants.rs
+++ src/constants.rs
@@ -1,7 +1,7 @@
 pub const USER_AGENT: &str =
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0";
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0";
 
-pub const PLAYER_VERSION: &str = "1.35.0-rc.2";
+pub const PLAYER_VERSION: &str = "1.40.0-rc.1";
 
 pub const TWITCH_GQL_ENDPOINT: &str = "https://gql.twitch.tv/gql";
 pub const TWITCH_OAUTH_ENDPOINT: &str = "https://id.twitch.tv/oauth2/validate";
